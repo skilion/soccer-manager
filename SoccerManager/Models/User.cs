ae8827812a33159ff340d8db519ce1b3260dd1dd
Andrea <andrea.9940@gmail.com>
2022-01-31 18:59:18 +0000

    Add Index to User Email

--- SoccerManager/Models/User.cs
+++ SoccerManager/Models/User.cs
@@ -1,9 +1,13 @@
-﻿namespace SoccerManager.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SoccerManager.Models
 {
+    [Index(nameof(Email))]
     public class User
     {
         public int UserId { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
         public int TeamId { get; set; }
 
         public Team Team { get; set; }

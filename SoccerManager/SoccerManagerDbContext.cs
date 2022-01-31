de8bd85864475d45292ec3a43e7fa36988552c15
Andrea <andrea.9940@gmail.com>
2022-01-31 13:29:07 +0000

    Add DbContext backed by SQLite

---
+++ SoccerManager/SoccerManagerDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoccerManager.Models;
+
+namespace SoccerManager
+{
+    public class SoccerManagerDbContext: DbContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Team> Teams { get; set; }
+        DbSet<Player> Player { get; set; }
+        DbSet<Transfer> Transfers { get; set; }
+
+        public SoccerManagerDbContext(DbContextOptions<SoccerManagerDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<Team>().ToTable("Team");
+            modelBuilder.Entity<Transfer>().ToTable("Transfer");
+        }
+    }
+}

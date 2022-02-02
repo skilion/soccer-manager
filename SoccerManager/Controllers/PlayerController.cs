24a1a095c494556fcf08db3452704d79fc7f91c8
Andrea <andrea.9940@gmail.com>
2022-02-02 12:21:13 +0000

    Create PlayerController

---
+++ SoccerManager/Controllers/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoccerManager.Models;
+
+namespace SoccerManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]/{id}")]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public class PlayerController : ControllerBase
+    {
+        private readonly SoccerManagerDbContext context;
+
+        public PlayerController(SoccerManagerDbContext context)
+        {
+            this.context = context;
+        }
+
+        /// <summary>
+        /// Get the details of a player.
+        /// </summary>
+        /// <returns>The team of the authenticated user</returns>
+        /// <response code="400">Bad Request</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="404">Not Found</response>
+        /// <response code="500">Internal Server Error</response>
+        [HttpGet]
+        [ProducesResponseType(typeof(Player), 200)]
+        public IActionResult Get([FromQuery] int id)
+        {
+            var player = LoadPlayer(id);
+            if (player is null)
+            {
+                return NotFound();
+            }
+            return Ok(player);
+        }
+
+        private Player? LoadPlayer(int id)
+        {
+            var player = context.Players.Where(player => player.PlayerId == id);
+            if (!player.Any())
+            {
+                return null;
+            }
+            return player.Single();
+        }
+    }
+}
---
+++ SoccerManagerTests/Controllers/PlayerControllerTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoccerManager.Controllers;
+using SoccerManager.Models;
+using SoccerManagerTests.Stubs;
+using System;
+using Xunit;
+
+namespace SoccerManagerTests.Controllers
+{
+    public class PlayerControllerTests : IDisposable
+    {
+        private readonly SoccerManagerDbContextStub context = new();
+        private readonly PlayerController controller;
+
+        public PlayerControllerTests()
+        {
+            controller = new PlayerController(context);
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        [Fact]
+        public void ShouldReturnPlayer()
+        {
+            // Arrange
+            int playerId = 1;
+            Player player = new()
+            {
+                PlayerId = playerId,
+                FirstName = "test",
+                LastName = "test",
+                Country = "test"
+            };
+            context.Players.Add(player);
+            context.SaveChanges();
+
+            // Act
+            var response = controller.Get(playerId);
+
+            // Assert
+            var result = Assert.IsType<OkObjectResult>(response);
+            var resultPlayer = Assert.IsAssignableFrom<Player>(result.Value);
+            Assert.Equal(player.PlayerId, resultPlayer.PlayerId);
+            Assert.Equal(player.FirstName, resultPlayer.FirstName);
+        }
+
+        [Fact]
+        public void ShouldReturnNotFound()
+        {
+            // Arrange
+            int playerId = 1;
+
+            // Act
+            var response = controller.Get(playerId);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(response);
+        }
+
+    }
+}

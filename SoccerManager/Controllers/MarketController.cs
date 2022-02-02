49c4abf7ea91a95a55d5d2b9059e2ee5438cb043
Andrea <andrea.9940@gmail.com>
2022-02-02 19:08:41 +0000

    Add Market Controller

---
+++ SoccerManager/Controllers/MarketController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SoccerManager.Models;
+
+namespace SoccerManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    public class MarketController : ControllerBase
+    {
+        private readonly SoccerManagerDbContext context;
+
+        public MarketController(SoccerManagerDbContext context)
+        {
+            this.context = context;
+        }
+
+        /// <summary>
+        /// Get the list of players on the market
+        /// </summary>
+        /// <response code="400">Bad Request</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="500">Internal Server Error</response>
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Transfer>), StatusCodes.Status200OK)]
+        public IActionResult Get()
+        {
+            var transfers = context.Transfers
+                .Include(transfer => transfer.Player)
+                .ToList();
+            return Ok(transfers);
+        }
+    }
+}
